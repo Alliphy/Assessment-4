665e4311b9f7e129fdfedf90fb7b56fd52fd4853
Alliphy <ruebaker29@gmail.com>
2024-06-20 18:47:29 -0600

    palette displaying

--- mindful-color-app/src/App.jsx
+++ mindful-color-app/src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
-import * as React from "react";
 import Glider from "react-glider";
 import "glider-js/glider.min.css";
-import fetchColors from "./server";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  let paletteColors;
+  const [paletteColors, setPaletteColors] = useState([]);
 
-  fetchColors();
+  useEffect(() => {
+    fetch("/api/random-palette")
+      .then((response) => response.json())
+      .then((data) => {
+        setPaletteColors(data.colors);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("paletteColors: ", paletteColors);
+  }, [paletteColors]);
 
   return (
     <>
@@ -22,25 +31,25 @@ export default function App() {
       </header>
 
       <main>
-        <div>
-          {paletteColors &&
-            paletteColors.length > 0 &&
-            paletteColors.map((color) => {
-              <div
-                style={{
-                  backgroundColor: color,
-                  width: "50px",
-                  height: "50px",
-                }}
-              ></div>;
-            })}
-        </div>
         <article>
-          <h1>Click submit to generate a Palette</h1>
-          <button type="submit">submit</button>
+          <button type="submit">Click to regenerate color palette</button>
         </article>{" "}
         <section className="App-Glider-Container">
           <h3>Palette</h3>
+
+          {paletteColors &&
+            paletteColors.length > 0 &&
+            paletteColors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  backgroundColor: `#${color}`,
+                  width: "50px",
+                  height: "50px",
+                }}
+              ></div>
+            ))}
+
           <Glider
             draggable
             hasArrows
@@ -53,50 +62,7 @@ export default function App() {
                 settings: { slidesToShow: 5 },
               },
             ]}
-          >
-            <article className="App-Card-Wrapper">
-              <div className="App-Card-Body">
-                <h4>lorem ipsum.</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud
-                </p>
-              </div>
-              <button className="App-Icons">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  style={{ width: "1.5rem", height: "1.5rem" }}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                  />
-                </svg>
-              </button>
-              <button className="App-Icons">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  style={{ width: "1.5rem", height: "1.5rem" }}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18 18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </article>
-          </Glider>
+          ></Glider>
         </section>
       </main>
       <footer></footer>
--- mindful-color-app/src/server.js
+++ mindful-color-app/src/server.js
@@ -2,10 +2,14 @@ import rgbHex from "rgb-hex";
 import express from "express";
 import session from "express-session";
 import morgan from "morgan";
+import axios from "axios";
+import cors from "cors";
 import ViteExpress from "vite-express";
 
+// MIDDLEWARE
+
 const app = express();
-const port = "8000";
+const port = "5173";
 ViteExpress.config({ printViteDevServerHost: true });
 
 app.use(morgan("dev"));
@@ -14,36 +18,38 @@ app.use(express.json());
 app.use(
   session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
 );
+app.use(cors());
 
-const convertRGBtoHex = async (color) => {
-  return rgbHex(color.toString());
-};
-
-const fetchColors = async () => {
+export const fetchColors = async () => {
   const url = "http://colormind.io/api/";
-  const request = {
+  const body = {
     model: "ui",
     input: ["N", "N", "N"],
   };
 
   try {
-    const response = await axios.post(url, JSON.stringify(request));
-
-    const paletteColors = response.data.result.map((color) => {
-      console.log("color: ", color);
-      // convert the color to a HEX value.
-      const hexValue = convertRGBtoHex(color);
-      console.log(hexValue);
+    const response = await axios.post(url, body);
+    const paletteColors = response.data.result.map(([r, g, b]) => {
+      const hexValue = rgbHex(r, g, b);
       return hexValue;
     });
     console.log("paletteColors: ", paletteColors);
+    return paletteColors;
   } catch (error) {
     console.error("Error fetching colors:", error);
   }
 };
 
+// app.get("/", (req, res) => {
+//   res.send("");
+// });
+
+app.get("/api/random-palette", async (req, res) => {
+  const colors = await fetchColors();
+  console.log(colors);
+  return res.json({ colors });
+});
+
 ViteExpress.listen(app, port, () =>
   console.log(`Server is listening on http://localhost:${port}`)
 );
-
-export default fetchColors;
